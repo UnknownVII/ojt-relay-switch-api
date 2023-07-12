3417a25f2660f25db317e41ccba77e06498e4d6f
John Wilbert Absalon <wilbz.absalon@gmail.com>
2023-07-12 23:40:39 +0800

    Added time and date channel object and endpoints

---
+++ src/routes/time.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const validateHmac = require("../../utils/global-hmac");
+const validateApiKey = require("../../utils/global-validate-api-key");
+const moment = require("moment-timezone");
+
+router.get("/current-time", validateHmac, validateApiKey, (req, res) => {
+  const currentDate = moment().tz("Asia/Manila").format("MM-DD-YYYY");
+  const currentTime = moment().tz("Asia/Manila").format("HH:mm");
+
+  res.json({ date: currentDate, time: currentTime });
+});
+
+module.exports = router;
